f1727b1515a4dcce544366cc8ea07ebf9597a9db
Yers <eabuov4@gmail.com>
2024-07-02 23:44:31 +0500

    prisma module, authdto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { authDto } from 'src/dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: authDto) {
+    console.log(dto);
     return this.authService.signup();
   }
 
